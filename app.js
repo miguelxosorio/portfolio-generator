7ed42bab2d350f1416e0dbe1a5ff0ef00c07fdee
vayne <miguel.osorio19@yahoo.com>
2022-01-09 14:13:40 -0600

    added validation on questions for inquirer method

--- app.js
+++ app.js
@@ -31,12 +31,33 @@ return inquirer.prompt([
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your Github Username'
+            message: 'Enter your Github Username',
+            validate: userName => {
+                if(userName) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Username');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if(confirmAbout) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -56,12 +77,26 @@ const promptProject = portfolioData => {
         {
           type: 'input',
           name: 'name',
-          message: 'What is the name of your project?'
+          message: 'What is the name of your project?',
+          validate: projectName => {
+              if(projectName) {
+                  return true;
+              } else {
+                  console.log('Please enter your project name');
+              } return false;
+          }
         },
         {
           type: 'input',
           name: 'description',
-          message: 'Provide a description of the project (Required)'
+          message: 'Provide a description of the project (Required)',
+          validate: projectDescription => {
+              if(projectDescription) {
+                  return true;
+              } else {
+                  console.log('Please write a description of your project');
+              } return false;
+          }
         },
         {
           type: 'checkbox',
@@ -72,7 +107,14 @@ const promptProject = portfolioData => {
         {
           type: 'input',
           name: 'link',
-          message: 'Enter the GitHub link to your project. (Required)'
+          message: 'Enter the GitHub link to your project. (Required)',
+          validate: gitHubLink => {
+              if(gitHubLink) {
+                  return true;
+              } else {
+                  console.log('Please provide a link for your GitHub');
+              } return false;
+          }
         },
         {
           type: 'confirm',
